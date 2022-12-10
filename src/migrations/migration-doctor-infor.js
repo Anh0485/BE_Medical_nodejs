29e0b990b1c891c57008c1eaae1cd8a8b66980e8
PhamVanAnh <contact.vananh0505@gmail.com>
2022-12-10 11:08:43 +0700

    first code

---
+++ src/migrations/migration-doctor-infor.js
@@ -0,0 +1,59 @@
+'use strict';
+// tự động match vào 
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('doctor_infor', {
+
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      doctorId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      priceId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      provinceId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      paymentId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      addressClinic: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      nameClinic:{
+        type: Sequelize.STRING,
+        allowNull:false,
+      }, 
+      note:{
+        type: Sequelize.STRING
+      },
+      count: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        default:0
+      },
+      createdAt:{
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updateAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+     
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('doctor_infor');
+  }
+};
